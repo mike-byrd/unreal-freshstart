128264c9aaa529d721bab659e95ecdf5074fc589
Mike Byrd <Mike Byrd>
2016-04-09 10:49:04 -0400

    Added FloatingActor

---
+++ Source/FreshStart/FloatingActor.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "FloatingActor.generated.h"
+
+UCLASS()
+class FRESHSTART_API AFloatingActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	float runningTime;
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloatingActor();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
