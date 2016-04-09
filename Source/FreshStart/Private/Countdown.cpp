288503893d61fd517d431577cc5711c7a01ad9b7
Mike Byrd <michaelcbyrdjr@gmail.com>
2016-04-09 12:52:29 -0400

    Added countdown

---
+++ Source/FreshStart/Private/Countdown.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FreshStart.h"
+#include "Countdown.h"
+
+
+// Sets default values
+ACountdown::ACountdown()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	countdownText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("CountdownNumber"));
+	countdownText->SetHorizontalAlignment(EHTA_Center);
+	countdownText->SetWorldSize(150.0f);
+	RootComponent = countdownText;
+
+	countdownTime = 3;
+}
+
+// Called when the game starts or when spawned
+void ACountdown::BeginPlay()
+{
+	Super::BeginPlay();
+	UpdateTimerDisplay();
+	GetWorldTimerManager().SetTimer(countdownTimerHandle, this, &ACountdown::AdvanceTimer, 1.0f, true);
+}
+
+// Called every frame
+void ACountdown::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void ACountdown::UpdateTimerDisplay()
+{
+	countdownText->SetText(FString::FromInt(FMath::Max(countdownTime, 0)));
+}
+
+void ACountdown::AdvanceTimer()
+{
+	--countdownTime;
+	UpdateTimerDisplay();
+	if (countdownTime < 1)
+	{
+		//done counting
+		GetWorldTimerManager().ClearTimer(countdownTimerHandle);
+		CountDownHasFinished();
+	}
+}
+
+void ACountdown::CountDownHasFinished()
+{
+	//Change to a special readout
+	countdownText->SetText("GO!");
+}
+
---
+++ Source/FreshStart/Public/Countdown.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Countdown.generated.h"
+
+UCLASS()
+class FRESHSTART_API ACountdown : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	int32 countdownTime;
+	FTimerHandle countdownTimerHandle;
+	UTextRenderComponent* countdownText;
+	
+public:	
+	// Sets default values for this actor's properties
+	ACountdown();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+	void UpdateTimerDisplay();
+	void AdvanceTimer();
+	void CountDownHasFinished();	
+	
+};
